c232ac123e9b51e1354a66a2047f4b40b53fde6b
packfan101 <jen20082@byui.edu>
2021-04-29 11:02:52 -0600

    .

--- js/date.js
+++ js/date.js
@@ -1,5 +1,5 @@
-let lastModified = document.lastModified;
-document.querySelector("#dateModified").textContent = lastModified;
-
 let year = new Date().getFullYear();
 document.querySelector("#currentYear").textContent = year;
+
+let lastModified = document.lastModified;
+document.querySelector("#dateModified").textContent = lastModified;
