7db2c571ad576e9e2b09114f2c12437a38fd1cf4
packfan101 <jen20082@byui.edu>
2021-06-24 17:23:13 -0600

    .

---
+++ lesson10/js/weatherSummary.js
@@ -0,0 +1,39 @@
+const citycode = document.querySelector('#citycode').textContent;
+const weatherURL = `http://api.openweathermap.org/data/2.5/weather?id=${citycode}&units=imperial&appid=a72098a69d05bfe82520977236405f51`;
+fetch(weatherURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
+    document.querySelector('#currentcondition').textContent = jsObject.weather[0].description;
+    document.querySelector('#currenttemp').textContent = jsObject.main.temp;
+    document.querySelector('#hightemp').textContent = jsObject.main.temp_max;
+    document.querySelector('#humidity').textContent = jsObject.main.humidity;
+    document.querySelector('#windspeed').textContent = jsObject.wind.speed;
+    windchill();
+  });
+
+
+const forecastURL = `http://api.openweathermap.org/data/2.5/forecast?id=${citycode}&units=imperial&appid=a72098a69d05bfe82520977236405f51`;
+fetch(forecastURL)
+  .then((response) => response.json())
+  .then((jsObject2) => {
+    console.log(jsObject2);
+    let day = 1;
+    const dayofweek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+    //reduce the list array to the five forecasts ... 40
+    const fiveDayForecast = jsObject2.list.filter( forecast => forecast.dt_txt.includes('18:00:00'));
+    console.log(fiveDayForecast);
+
+    //Loop through each of the forecast days
+    fiveDayForecast.forEach( x => {
+      let d = new Date(x.dt_txt);
+      document.querySelector(`#day${day}`).textContent = dayofweek[d.getDay()];
+      document.querySelector(`#temp${day}`).textContent = x.main.temp;
+      const imagesrc = 'https://openweathermap.org/img/w/' + x.weather[0].icon + '.png';
+      const desc = x.weather[0].description;
+      document.querySelector(`#weathericon${day}`).setAttribute('src', imagesrc);
+      document.querySelector(`#weathericon${day}`).setAttribute('alt', desc);
+      day++
+    });
+  });
