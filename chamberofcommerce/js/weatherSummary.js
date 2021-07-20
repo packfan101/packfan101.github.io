20e37fd6e7554dac4111c155d4361b55d1e7490a
packfan101 <jen20082@byui.edu>
2021-07-20 10:52:47 -0600

    .

--- chamberofcommerce/js/weatherSummary.js
+++ chamberofcommerce/js/weatherSummary.js
@@ -1,10 +1,11 @@
 const lat = document.querySelector('.weathsumhead').dataset["lat"];
 const lon = document.querySelector('.weathsumhead').dataset["lon"];
 
-const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=a72098a69d05bfe82520977236405f51`
+const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=a72098a69d05bfe82520977236405f51`
 fetch(weatherURL)
   .then((response) => response.json())
   .then((jsObject) => {
+    console.log(jsObject);
     document.querySelector('#currentcondition').textContent = jsObject.current.weather[0].main;
     document.querySelector('#currenttemp').textContent = jsObject.current.temp.toFixed(0);
     document.querySelector('#hightemp').textContent = jsObject.daily[0].temp.max.toFixed(0);
@@ -21,5 +22,48 @@ fetch(weatherURL)
       document.querySelector(`#weathericon${day}`).setAttribute('alt', desc);
       day++
     }
+    if (typeof jsObject.alerts !== 'undefined') {
+      let h2 = document.createElement('h2');
+      h2.textContent = `Weather Alerts for West Jordan`;
+      document.querySelector('div.weatheralert').appendChild(h2);
+
+      for (let i=0; i < jsObject.alerts.length; i++) {        
+        let alert = document.createElement('ul');
+        let event = document.createElement('li');
+        let description = document.createElement('li');
+        //let start = document.createElement('li');
+        //let end = document.createElement('li');
+
+        event.textContent = `${jsObject.alerts[i].event}`;
+        description.textContent = `${jsObject.alerts[i].description}`; 
+        //start.textContent = `Beginning: ${new Date(jsObject.alerts[i].start * 1000)}`;
+        //end.textContent = `Ending: ${new Date(jsObject.alerts[i].end * 1000)}`;        
+
+        alert.appendChild(event);
+        alert.appendChild(description);
+        //alert.appendChild(start);
+        //alert.appendChild(end);
+        alert.setAttribute('class', 'alert');
+        
+        document.querySelector('div.weatheralert').appendChild(alert);
+      }
+    }
+    else {
+      let h2 = document.createElement('h2');
+      h2.textContent = `Weather Alerts for West Jordan`;
+      document.querySelector('div.weatheralert').appendChild(h2);
+      let alert = document.createElement('ul');
+      let event = document.createElement('li');
+      event.textContent = `No Current Weather Alerts`;
+      alert.appendChild(event);
+      document.querySelector('div.weatheralert').appendChild(alert);
+    };
   });
+
+const closebutton = document.querySelector('.closebutton');
+const container = document.querySelector('.weatheralert');
+
+closebutton.addEventListener('click', () => {
+  container.classList.add('hidealert');
+})
 
